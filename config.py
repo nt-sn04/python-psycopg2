9e86be047edc0fc6ad4d1773cf835f49ce5b1128
djumanov <djumanovdev@gmail.com>
2026-02-26 19:34:47 +0500

    conntection

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class Settings:
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASS = os.getenv("DB_PASS")
+    DB_NAME = os.getenv("DB_NAME")
+
+
+settings = Settings()
